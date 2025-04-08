a3250ce65297d8a29840b094b364cc8d97bf6aca
ChaoticHamka <chaotic.hamka@gmail.com>
2025-04-08 21:58:06 +0300

    add тесты в CalculatorTest + refactor

--- src/main/java/by/itacademy/radzetskaya/Calculator.java
+++ src/main/java/by/itacademy/radzetskaya/Calculator.java
@@ -2,22 +2,22 @@ package by.itacademy.radzetskaya;
 
 public class Calculator {
 
-    public static int sum(int firstNumber, int secondNumber){
+    public static int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
 
-    public static int multiplication (int firstNumber, int secondNumber){
+    public static int multiply(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
     }
 
-    public static int diviision (int firstNumber, int secondNumber){
-        if (secondNumber == 0){
+    public static int divide(int firstNumber, int secondNumber) {
+        if (secondNumber == 0) {
             return 0;
         }
         return firstNumber / secondNumber;
     }
 
-    public static int difference (int firstNumber, int secondNumber){
+    public static int subtract(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
 }
--- src/test/java/by/itacademy/radzetskaya/CalculatorTest.java
+++ src/test/java/by/itacademy/radzetskaya/CalculatorTest.java
@@ -7,7 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
 
     @Test
-    public void testSum(){
-        assertEquals(2, Calculator.sum(2,1));
+    public void testSum() {
+        assertEquals(3, Calculator.sum(2, 1));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(2, Calculator.multiply(2, 1));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, Calculator.divide(2, 1));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, Calculator.subtract(2, 1));
     }
 }
