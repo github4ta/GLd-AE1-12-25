0dec4cf5178dbe9923364731bd902b30ec53dcbc
RomanKuleshov <kuleshovqa@gmail.com>
2025-04-09 19:38:31 +0300

    refactor Calculator and CalculatorTest classes.

--- src/main/java/by/itacademy/Calculator.java
+++ src/main/java/by/itacademy/Calculator.java
@@ -1,4 +1,12 @@
 package by.itacademy;
 
 public class Calculator {
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
 }
--- src/test/java/by/itacademy/CalculatorTest.java
+++ src/test/java/by/itacademy/CalculatorTest.java
@@ -1,4 +1,28 @@
 package by.itacademy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    @DisplayName("Проверка метода деление с переменными типа int")
+    public void testDivideIntValues() {
+        Calculator calculator = new Calculator();
+        long actual = calculator.divide(27, 3);
+
+        assertEquals(9, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка метода деление на ноль с переменными типа int")
+    public void testDivideByZeroIntValues() {
+        Calculator calculator = new Calculator();
+        long actual = calculator.divide(27, 0);
+
+        assertEquals(0, actual);
+    }
+
 }
