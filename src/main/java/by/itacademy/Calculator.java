69ae608c0064003906076cdbcf62df9801a413fe
i.evminova <evminova.iryna@gmail.com>
2025-04-09 19:43:49 +0300

    add calculator class, test Evminova

--- src/main/java/by/itacademy/Calculator.java
+++ src/main/java/by/itacademy/Calculator.java
@@ -1,4 +1,7 @@
 package by.itacademy;
 
 public class Calculator {
+    public double divide(double a, double b){
+        return a / b;
+    }
 }
--- src/test/java/by/itacademy/CalculatorTest.java
+++ src/test/java/by/itacademy/CalculatorTest.java
@@ -1,4 +1,16 @@
 package by.itacademy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        double actual = calculator.divide(95151, 58587);
+
+        assertEquals(1.624, actual, 0.001);
+    }
 }
