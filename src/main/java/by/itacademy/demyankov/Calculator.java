b379368d6afdfd8efa2d2158294321447af44b7c
Demyankov Viktor <vitya.demyankov@gmail.com>
2025-04-09 19:46:54 +0300

    Home refaktor

--- src/main/java/by/itacademy/demyankov/Calculator.java
+++ src/main/java/by/itacademy/demyankov/Calculator.java
@@ -15,6 +15,9 @@ public class Calculator {
     }
 
     public int dividing(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
         return a / b;
     }
 
--- src/test/java/by/itacademy/demyankov/CalculatorTest.java
+++ src/test/java/by/itacademy/demyankov/CalculatorTest.java
@@ -11,29 +11,29 @@ public class CalculatorTest {
     @DisplayName("Сумирования")
     public void testAdd() {
         Calculator calculator = new Calculator();
-              assertEquals(18,calculator.add(5.2,12.8));
+        assertEquals(18, calculator.add(5.2, 12.8));
     }
 
     @Test
     @DisplayName("Вычитание")
     public void testsubtraction() {
         Calculator calculator = new Calculator();
-        assertEquals(99,calculator.subtraction(100,1));
+        assertEquals(99, calculator.subtraction(100, 1));
     }
 
 
     @Test
     @DisplayName("Умножение")
-    public void testMultiplay(){
+    public void testMultiplay() {
         Calculator calculator = new Calculator();
-        assertEquals(10,calculator.multiplay(5,2));
+        assertEquals(10, calculator.multiplay(5, 2));
     }
 
     @Test
     @DisplayName("Деление")
-    public void testDivinding(){
+    public void testDivinding() {
         Calculator calculator = new Calculator();
-        assertEquals(5,calculator.dividing(10,2));
+        assertEquals(5, calculator.dividing(10, 2));
     }
 
 }
