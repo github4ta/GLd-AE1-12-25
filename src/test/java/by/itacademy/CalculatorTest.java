cff5ac5ce0944cc1b2c0ce77d5fa1b20978ecb6a
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-04-09 19:35:49 +0300

    add method subtract and testSubtract

--- src/test/java/by/itacademy/CalculatorTest.java
+++ src/test/java/by/itacademy/CalculatorTest.java
@@ -1,4 +1,14 @@
 package by.itacademy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    @Test
+    public void testSubtract(){
+        Calculator calculator = new Calculator();
+        int actual = calculator.subtract(4,2);
+        assertEquals (2, actual);
+    }
 }
