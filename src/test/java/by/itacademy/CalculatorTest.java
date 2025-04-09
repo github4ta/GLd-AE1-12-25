48b9c47ccb88f00d65ddf98e07089a41259c6ac4
a.romanovska <a.romanovska.dev@gmail.com>
2025-04-09 19:32:46 +0300

    add sum(int,int) test

--- src/test/java/by/itacademy/CalculatorTest.java
+++ src/test/java/by/itacademy/CalculatorTest.java
@@ -1,4 +1,13 @@
 package by.itacademy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    @Test
+    public void testSum() {
+        int actual = Calculator.sum(5, 2);
+        assertEquals(7, actual);
+    }
 }
