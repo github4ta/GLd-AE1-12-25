fca88af136ac4a444984e8f0fd9a39b981e55ddf
Victor Sikachyov <3510154@gmail.com>
2025-04-09 19:47:27 +0300

    add multiply for integer

--- src/test/java/by/itacademy/CalculatorTest.java
+++ src/test/java/by/itacademy/CalculatorTest.java
@@ -1,4 +1,15 @@
 package by.itacademy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(2, 3);
+        assertEquals(6, result);
+    }
 }
