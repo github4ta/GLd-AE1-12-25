2b3d70b0871ed1005949f2e173fd4676e882be2f
Eva <evaprigoghaeva@gmail.com>
2025-04-09 19:36:49 +0300

    add BayarankaYauheniya

--- src/test/java/by/itacademy/CalculatorTest.java
+++ src/test/java/by/itacademy/CalculatorTest.java
@@ -1,4 +1,15 @@
 package by.itacademy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.subtract(3, 1);
+
+        assertEquals(2, actual);
+    }
 }
