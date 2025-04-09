d6e91cb910f9f21851fde53125e8b2af13e04ec3
RomanKuleshov <kuleshovqa@gmail.com>
2025-04-09 12:04:25 +0300

    refactor Calculator class, add additional test min and max methods in CalculatorTest class.

--- src/main/java/by/itacademy/kuleshov/Calculator.java
+++ src/main/java/by/itacademy/kuleshov/Calculator.java
@@ -3,22 +3,64 @@ package by.itacademy.kuleshov;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        long result = (long) a + b;
+
+        if (result > Integer.MAX_VALUE) {
+            result = Integer.MAX_VALUE;
+        }
+
+        if (result < Integer.MIN_VALUE) {
+            result = Integer.MIN_VALUE;
+        }
+
+        return (int) result;
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        long result = (long) a - b;
+
+        if (result > Integer.MAX_VALUE) {
+            result = Integer.MAX_VALUE;
+        }
+
+        if (result < Integer.MIN_VALUE) {
+            result = Integer.MIN_VALUE;
+        }
+
+        return (int) result;
     }
 
     public int multiply(int a, int b) {
-        return a * b;
+        long result = (long) a * b;
+
+        if (result > Integer.MAX_VALUE) {
+            result = Integer.MAX_VALUE;
+        }
+
+        if (result < Integer.MIN_VALUE) {
+            result = Integer.MIN_VALUE;
+        }
+
+        return (int) result;
     }
 
-    public int divide(int a, int b) {
-       if (b == 0) {
-           return -1;
-       } else {
-           return a / b;
-       }
+    public int divide(long a, long b) {
+        long result = 0;
+
+        if (b == 0) {
+            result = 0;
+        } else {
+            result = a / b;
+        }
+
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (int) result;
     }
 }
--- src/test/java/by/itacademy/kuleshov/CalculatorTest.java
+++ src/test/java/by/itacademy/kuleshov/CalculatorTest.java
@@ -1,38 +1,114 @@
 package by.itacademy.kuleshov;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CalculatorTest {
 
     @Test
+    @Order(1)
+    @DisplayName("Проверка метода Сложение")
     public void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(8, calculator.add(3, 5));
     }
 
     @Test
+    @Order(2)
+    @DisplayName("Проверка метода Сложение (Максимальное значение)")
+    public void testAddMax() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MAX_VALUE, calculator.add(Integer.MAX_VALUE, 1));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Проверка метода Сложение (Минимальное значение)")
+    public void testAddMin() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MIN_VALUE, calculator.add(Integer.MIN_VALUE, -1));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Проверка метода Вычитание")
     public void testSubtract() {
         Calculator calculator = new Calculator();
         assertEquals(15, calculator.subtract(19, 4));
     }
 
     @Test
+    @Order(5)
+    @DisplayName("Проверка метода Вычитание (Максимальное значение)")
+    public void testSubtractMax() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MAX_VALUE, calculator.subtract(Integer.MAX_VALUE, -1));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Проверка метода Вычитание (Минимальное значение)")
+    public void testSubtractMin() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MIN_VALUE, calculator.subtract(Integer.MIN_VALUE, 1));
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Проверка метода Умножение")
     public void testMultiply() {
         Calculator calculator = new Calculator();
         assertEquals(10, calculator.multiply(2, 5));
     }
 
     @Test
+    @Order(8)
+    @DisplayName("Проверка метода Умножение (Максимальное значение)")
+    public void testMultiplyMax() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MAX_VALUE, calculator.multiply(Integer.MAX_VALUE, 2));
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Проверка метода Умножение (Минимальное значение)")
+    public void testMultiplyMin() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MIN_VALUE, calculator.multiply(Integer.MIN_VALUE, 2));
+    }
+
+
+    @Test
+    @Order(10)
+    @DisplayName("Проверка метода Деление")
     public void testDivide() {
         Calculator calculator = new Calculator();
         assertEquals(3, calculator.divide(15, 5));
     }
 
     @Test
+    @Order(11)
+    @DisplayName("Проверка метода Деление (Максимальное значение)")
+    public void testDivideMax() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MAX_VALUE, calculator.divide(Integer.MAX_VALUE * 3L, 2));
+    }
+
+    @Test
+    @Order(12)
+    @DisplayName("Проверка метода Деление (Минимальное значение)")
+    public void testDivideMin() {
+        Calculator calculator = new Calculator();
+        assertEquals(Integer.MIN_VALUE, calculator.divide(Integer.MIN_VALUE * 3L, 2));
+    }
+
+    @Test
+    @Order(13)
+    @DisplayName("Проверка метода Деление, деление на ноль")
     public void testDivideByZero() {
         Calculator calculator = new Calculator();
-        assertEquals(-1, calculator.divide(15, 0));
+        assertEquals(0, calculator.divide(15L, 0L));
     }
 }
