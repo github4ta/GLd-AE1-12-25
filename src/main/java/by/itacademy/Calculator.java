2a472665fe48c7ed2922f9dd795c695b57a8f0f9
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-04-09 20:52:06 +0300

    refactor code

--- src/main/java/by/itacademy/Calculator.java
+++ src/main/java/by/itacademy/Calculator.java
@@ -3,9 +3,8 @@ package by.itacademy;
 public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
-    public int subtract(int a, int b) {
-        return a - b;
- }
+    }
+
     public double summDouble(double a, double b) {
         return a + b;
     }
