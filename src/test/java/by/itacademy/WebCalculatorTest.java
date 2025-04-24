9cc5c2194d246bcb4fd635b97b1f2b8d25d938e7
Sergei Tsarik <serge.tsarik@gmail.com>
2025-04-24 22:12:01 +0300

    add selenium

---
+++ src/test/java/by/itacademy/WebCalculatorTest.java
@@ -0,0 +1,35 @@
+package by.itacademy;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebCalculatorTest {
+
+    @Test
+    public void test1() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        WebElement nameInput = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input"));
+        nameInput.sendKeys("John");
+
+        WebElement heightInput = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input"));
+        heightInput.sendKeys("180");
+
+        WebElement weightInput = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input"));
+        weightInput.sendKeys("80");
+
+        WebElement genderMale = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]"));
+        genderMale.click();
+
+        WebElement buttonCalculate = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input"));
+        buttonCalculate.click();
+
+        // actual text
+
+        // Идеальная масса тела
+    }
+}
