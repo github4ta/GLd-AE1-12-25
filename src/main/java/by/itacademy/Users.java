3e1939a0c16517dfaabaf7fcf982543cdc670ced
Lexzorg1990 <alexandr.pesetskiy@gmail.com>
2025-04-24 19:57:08 +0300

    add avg

---
+++ src/main/java/by/itacademy/Users.java
@@ -0,0 +1,14 @@
+package by.itacademy;
+
+import java.util.List;
+
+public class Users {
+    public static double getAverageGrade(List<User> users) {
+        int sum = 0;
+        for (User user : users) {
+            sum += user.getGrade();
+        }
+        double averageGrade = sum / users.size();
+        return averageGrade;
+    }
+}
