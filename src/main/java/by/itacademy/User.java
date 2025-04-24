20bbd5f5e0b82b9153e1cab7512dc34ebf285967
Юлия <chaotic.hamka@gmail.com>
2025-04-24 19:41:06 +0300

    refactor пустую строку

--- src/main/java/by/itacademy/User.java
+++ src/main/java/by/itacademy/User.java
@@ -3,11 +3,11 @@ package by.itacademy;
 import java.util.Objects;
 
 public class User {
+
     private String name;
     private int grade;
 
     public User(String name, int grade) {
-
         this.name = name;
         this.grade = grade;
     }
