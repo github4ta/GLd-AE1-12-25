eb18b38cf43451f58e1569a1e96818ec23e9119d
Sergei Tsarik <serge.tsarik@gmail.com>
2025-05-08 20:27:17 +0300

    add test3

--- src/test/java/by/itacademy/OnlinerTest.java
+++ src/test/java/by/itacademy/OnlinerTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -79,6 +80,42 @@ public class OnlinerTest {
         assertEquals("Информация о компании", aboutCompanyTitleText);
 
         //закрыть браузер
+
+    }
+
+    @Test
+    public void test3() {
+        // 1. Открыть браузер.
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 2. Ввести URL сайта.
+        driver.get("https://www.onliner.by");
+
+        // 3. Принять куки
+        String xpath = "//a[@id='submit-button']";
+        WebElement submitButton = driver.findElement(By.xpath(xpath));
+        submitButton.click();
+
+        // ожидания появления элемента "О компании"
+        // Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        xpath = "//a[@href='https://blog.onliner.by/about']";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Ждем появления элемента и заново его находим
+        WebElement aboutCompany = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+        aboutCompany.click();
+
+        // 4. Проверить заголовок "Информация о компании"
+        xpath = "//h1[text()[contains(.,'Информация о компании')]]";
+
+        // Ждем появления заголовка и заново его находим
+        WebElement aboutCompanyTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        String aboutCompanyTitleText = aboutCompanyTitle.getText();
+
+        assertEquals("Информация о компании", aboutCompanyTitleText);
+        // 5. Закрыть браузер.
+        driver.quit();
 
     }
 }
