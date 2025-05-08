dac88575002090b5a24a7f6e41bb6afe90557c07
Sergei Tsarik <serge.tsarik@gmail.com>
2025-05-08 21:37:35 +0300

    refactor

--- src/test/java/by/itacademy/VekTest.java
+++ src/test/java/by/itacademy/VekTest.java
@@ -1,6 +1,8 @@
 package by.itacademy;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,9 +10,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class VekTest {
-    @Test
-    public void test1() {
-        WebDriver driver = new ChromeDriver();
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://www.21vek.by");
@@ -18,7 +23,10 @@ public class VekTest {
         String submitButtonCookieLocator = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
         WebElement submitButtonCookie = driver.findElement(By.xpath(submitButtonCookieLocator));
         submitButtonCookie.click();
+    }
 
+    @Test
+    public void test1() {
         String linAllActionsLocator = "//a[@href=\"/special_offers/promo.html\"]";
         WebElement linkAllActions = driver.findElement(By.xpath(linAllActionsLocator));
         linkAllActions.click();
@@ -28,27 +36,19 @@ public class VekTest {
         String actual = allActionsTitle.getText();
 
         Assert.assertEquals("Все акции", actual);
-
-        driver.quit();
     }
 
     @Test
     public void test2() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://www.21vek.by");
-
-        String submitButtonCookieLocator = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
-        WebElement submitButtonCookie = driver.findElement(By.xpath(submitButtonCookieLocator));
-        submitButtonCookie.click();
-
         String legalInformationBlockLocator = "//div[@class=\"styles_legalInformationBlock__iXOVK\"]";
         WebElement legalInformationBlock = driver.findElement(By.xpath(legalInformationBlockLocator));
         String actual = legalInformationBlock.getText();
 
         Assert.assertTrue(actual.contains("© 2004–2025 21vek.by,"));
+    }
 
+    @AfterEach
+    public void tearDown() {
         driver.quit();
     }
 }
