e8cad686b00f290d18737d3628c6d0ecea43e906
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-05-11 19:16:56 +0300

    add BaseSeleniumTest class

---
+++ src/test/java/core/BaseSeleniumTest.java
@@ -0,0 +1,12 @@
+package core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class BaseSeleniumTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+
+
+}
