6fee6e400922ba40a40689243bab9eb4dbbe6e44
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-05-11 19:23:57 +0300

    add BaseSeleniumTest class

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -1,12 +1,31 @@
 package core;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseSeleniumTest {
     protected WebDriver driver;
 
     @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.21vek.by");
+        String submitButtonCookieLocator = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
+        WebElement submitButtonCookie = driver.findElement(By.xpath(submitButtonCookieLocator));
+        submitButtonCookie.click();
+    }
 
-
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
 }
