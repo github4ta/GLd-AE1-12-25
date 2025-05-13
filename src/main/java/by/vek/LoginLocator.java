e579be0c1a97365aa28d7865d6ce1c5898792aa0
Alexandr Ivkin <alexandr1ivkin@gmail.com>
2025-05-13 20:08:04 +0300

    add class VekTest (none optimise code)

--- src/main/java/by/vek/LoginLocator.java
+++ src/main/java/by/vek/LoginLocator.java
@@ -1,15 +1,20 @@
 package by.vek;
 
 public class LoginLocator {
-    public final static String SUBMIT_BUTTON_COOKIE = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
-    public final static String LINK_ALL_ACTIONS = "//a[@href=\"/special_offers/promo.html\"]";
-    public final static String ALL_ACTIONS_TITLE = "//h1[@class=\"style_title__c0jN1 Title-module__title Title-module__h1\"]";
-    public final static String LEGAL_INFORMATION_BLOCK = "//div[@class=\"styles_legalInformationBlock__iXOVK\"]";
-    public final static String BUTTON_USER_ACCOUNT = "//button[@class=\"styles_userToolsToggler__c2aHe\"]";
-    public final static String TITLE_LOGIN_FORM = "//div[@class=\"LoginForm_title__OAEXy Text-module__text Text-module__large Text-module__bold\"]";
-    public final static String RADIO_BUTTON_EMAIL = "//input[@type='checkbox' and @checked]/following-sibling::span[contains(text(), 'Электронной почте')]/ancestor::label";
-    public final static String BUTTON_LOGIN_SUBMIT = "//button[@data-testid=\"loginSubmit\"]";
-    public final static String VALIDATION_ERROR_MESSAGE_EMAIL = "//span[contains(@class, 'message') and contains(., 'не указана')]";
-    public final static String VALIDATION_ERROR_MESSAGE_PASSWORD = "//span[contains(@class, 'message') and contains(., 'Пароль')]";
-    public final static String LINK_BUTTON_REGISTRATION_OR_RESET_PASSWORD = "//a[@class=\"LinkButton-module__wrapper LinkButton-module__tiny LinkButton-module__regular LinkButton-module__blue\"]";
+    public static final String BUTTON_USER_ACCOUNT = "//button[@class=\"styles_userToolsToggler__c2aHe\"]";
+    public static final String BUTTON_LOGIN = "//button[@data-testid=\"loginButton\"]";
+    public static final String RADIO_BUTTON_CHOOSE_EMAIL = "//label[@class=\"BaseRadioButton-module__label BaseRadioButton-module__checkedLabel\"]";
+    public static final String SUBMIT_BUTTON = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
+    public static final String INPUT_EMAIL_FIELD = "//input[@id=\"login-email\"]";
+    public static final String INPUT_PASSWORD_FIELD = "//input[@id=\"login-password\"]";
+    public static final String BUTTON_CONTINUE = "//button[@data-testid=\"loginSubmit\"]";
+    public static final String LINK_ALL_ACTIONS = "//a[@href=\"/special_offers/promo.html\"]";
+    public static final String ALL_ACTION_TITLE = "//h1[@class=\"style_title__c0jN1 Title-module__title Title-module__h1\"]";
+    public static final String LEGAL_INFORMATION_BLOCK = "//div[@class=\"styles_legalInformationBlock__iXOVK\"]";
+    public static final String TITLE_LOGIN_FORM = "//div[@class=\"LoginForm_title__OAEXy Text-module__text Text-module__large Text-module__bold\"]";
+    public static final String ERROR_MESSAGE_EMAIL_INPUT = "//span[text()[contains(.,'Электронная почта не указана')]]";
+    public static final String ERROR_MESSAGE_PASSWORD_INPUT = "//span[text()[contains(.,'Пароль не указан')]]";
+    public static final String ERROR_MESSAGE_NO_REGISTER_EMAIL_INPUT = "//span[@class=\"ErrorMessageLink_container__7D0yM\"]";
+    public static final String LINK_BUTTON_REGISTRATION = "//a[@class=\"LinkButton-module__wrapper LinkButton-module__tiny LinkButton-module__regular LinkButton-module__blue\"]";
+    public static final String TITLE_REGISTRATION = "//h5[@class=\"Form-module__formTitle\"]";
 }
