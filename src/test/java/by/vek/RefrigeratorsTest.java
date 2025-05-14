1e3b57bd5d9b0b21e5d4da3518d7ff5a9d9f43c1
Alexandr Ivkin <alexandr1ivkin@gmail.com>
2025-05-14 22:47:21 +0300

    add classes RefrigeratorsLocator, RefrigeratorsPage and RefrigeratorsTest

---
+++ src/test/java/by/vek/RefrigeratorsTest.java
@@ -0,0 +1,48 @@
+package by.vek;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class RefrigeratorsTest {
+
+    private WebDriver driver;
+    private RefrigeratorsPage refrigeratorsPage;
+    private LoginPage loginPage;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        driver.get("https://www.21vek.by");
+        refrigeratorsPage = new RefrigeratorsPage(driver);
+        refrigeratorsPage.clickSubmitButtonCookie();
+        refrigeratorsPage.clickLinkRefrigerators();
+    }
+
+    @Test
+    public void checkTitleRefrigeratorsText() {
+        Assertions.assertEquals("Холодильники", refrigeratorsPage.getTitleRefrigeratorsPageText());
+    }
+
+    @Test
+    public void checkAdditionGoodsLinkText() {
+        Assertions.assertEquals("Морозильники", refrigeratorsPage.getLinkFreezersText());
+        Assertions.assertEquals("Холодильники встраиваемые", refrigeratorsPage.getLinkBuiltinRefrigeratorsText());
+        Assertions.assertEquals("Автохолодильники", refrigeratorsPage.getLinkBagsRefrigeratorsText());
+        Assertions.assertEquals("Морозильные лари", refrigeratorsPage.getLinkFreezersLariText());
+        Assertions.assertEquals("Магниты грифельные", refrigeratorsPage.getLinkSlateMagnetsText());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
