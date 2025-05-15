7b19b41ab01ee8d15c255d0123d876def83a7714
alex-1i <94687457+alex-1i@users.noreply.github.com>
2025-05-15 21:08:04 +0300

    UI0004
    * add classes RefrigeratorsLocator, RefrigeratorsPage and RefrigeratorsTest
    
    * Update RefrigeratorsPage.java
    
    * Update RefrigeratorsLocator.java

---
+++ src/main/java/by/vek/RefrigeratorsLocator.java
@@ -0,0 +1,11 @@
+package by.vek;
+
+public class RefrigeratorsLocator {
+    public static final String LINK_REFRIGERATORS = "//a[@href=\"/refrigerators/\"]";
+    public static final String TITLE_REFRIGERATORS_PAGE = "//h1[@class=\"style_title__QomyX\"]";
+    public static final String LINK_FREEZERS = "//a[@href=\"/freezers/\"]";
+    public static final String LINK_BUILTIN_REFRIGERATORS = "//a[@href=\"/builtin_refrigerators/\"]";
+    public static final String LINK_BAGS_REFRIGERATORS = "//a[@href=\"/bags_refrigerators/20154/\"]";
+    public static final String LINK_FREEZERS_LARI = "//a[@href=\"/freezers/303184/\"]";
+    public static final String LINK_SLATE_MAGNETS = "//a[@href=\"/easels_drawing_boards/778319/\"]";
+}
---
+++ src/main/java/by/vek/RefrigeratorsPage.java
@@ -0,0 +1,48 @@
+package by.vek;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static by.vek.RefrigeratorsLocator.*;
+public class RefrigeratorsPage {
+
+    private WebDriver driver;
+
+    public RefrigeratorsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickSubmitButtonCookie() {
+        WebElement submitButtonCookie = driver.findElement(By.xpath(LoginLocator.SUBMIT_BUTTON_COOKIE));
+        submitButtonCookie.click();
+    }
+
+    public void clickLinkRefrigerators() {
+        driver.findElement(By.xpath(LINK_REFRIGERATORS)).click();
+    }
+
+    public String getTitleRefrigeratorsPageText() {
+        return driver.findElement(By.xpath(TITLE_REFRIGERATORS_PAGE)).getText();
+    }
+
+    public String getLinkFreezersText() {
+        return driver.findElement(By.xpath(LINK_FREEZERS)).getText();
+    }
+
+    public String getLinkBuiltinRefrigeratorsText() {
+        return driver.findElement(By.xpath(LINK_BUILTIN_REFRIGERATORS)).getText();
+    }
+
+    public String getLinkBagsRefrigeratorsText() {
+        return driver.findElement(By.xpath(LINK_BAGS_REFRIGERATORS)).getText();
+    }
+
+    public String getLinkFreezersLariText() {
+        return driver.findElement(By.xpath(LINK_FREEZERS_LARI)).getText();
+    }
+
+    public String getLinkSlateMagnetsText() {
+        return driver.findElement(By.xpath(LINK_SLATE_MAGNETS)).getText();
+    }
+}
