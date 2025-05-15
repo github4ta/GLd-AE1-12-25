bdf5a2d88da8c846f283e7b8f2233276e81d2394
a.romanovska <a.romanovska.dev@gmail.com>
2025-05-15 21:31:20 +0300

    add MyDriver class

--- src/main/java/by/vek/driver/MyDriver.java
+++ src/main/java/by/vek/driver/MyDriver.java
@@ -1,4 +1,28 @@
 package by.vek.driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class MyDriver {
+    private static WebDriver driver;
+
+    private MyDriver() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        }
+        return driver;
+    }
+
+    public static void quit() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
