2be4f4ba8077ce95ed367cc5c097e681ab65e55a
Юлия <chaotic.hamka@gmail.com>
2025-05-15 06:39:40 +0300

    add VacuumTest, VacuumPage, VacuumLocator

---
+++ src/main/java/by/vek/VacuumLocator.java
@@ -0,0 +1,11 @@
+package by.vek;
+
+public class VacuumLocator {
+    public static final String SUBMIT_BUTTON_COOKIE = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
+    public static final String CATALOG_CATEGORY = "//span[contains(text(), \"Каталог товаров\")]";
+    public static final String CATEGORY_VACUUM_LINK = "//a[@href=\"/vacuum/\"]/span";
+    public static final String TITLE_VACUUM = "//h1[contains(text(), 'Пылесосы')]";
+    public static final String VACUUM_ATTACHMENTS_LINK = "//a[@href=\"/vacuum_attachments/\"]";
+    public static final String VACUUM_FILTERS_BAGS_LINK = "//a[@href=\"/vacuum_filters_bags/\"]";
+    public static final String VACUUM_ACCESSORIES_LINK = "//a[@href=\"/vacuum_accessories/\"]";
+}
