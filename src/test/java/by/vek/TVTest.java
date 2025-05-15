4d7070130220b8943bee33b8f3fa033c0835316f
romankuleshov <kuleshov.roman96@gmail.com>
2025-05-15 16:42:29 +0300

    add TVLocator, TVPage and TVTest classes.

---
+++ src/test/java/by/vek/TVTest.java
@@ -0,0 +1,72 @@
+package by.vek;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TVTest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private TVPage tvPage;
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.21vek.by");
+
+        loginPage = new LoginPage(driver);
+        loginPage.clickSubmitButtonCookie();
+
+        tvPage = new TVPage(driver);
+        tvPage.clickTVCategoryHeaderButton();
+    }
+
+    @Test
+    @DisplayName("TV page - Verify title text")
+    public void testTVPageTitleText() {
+
+        assertEquals("Телевизоры", tvPage.getTextTVPageTitle());
+    }
+
+    @Test
+    @DisplayName("TV page - Verify titles text in content block")
+    public void testTVPageCategoryContentTitleText() {
+
+        assertTrue(tvPage.isCategoryContentTitlesContainText("Особенности телевизоров"));
+        assertTrue(tvPage.isCategoryContentTitlesContainText("Телевизор для дачи"));
+        assertTrue(tvPage.isCategoryContentTitlesContainText("Какой телевизор купить в Минске?"));
+    }
+
+    @Test
+    @DisplayName("TV page - Verify that all items contains category text in the name")
+    public void testTVPageItemsNamesText() {
+
+        assertTrue(tvPage.areTVPageItemsContainText("Телевизор"));
+    }
+
+    @Test
+    @DisplayName("TV page - Verify that all items contains selected brand text in the name")
+    public void testTVPageManufacturerFilter() throws InterruptedException {
+        String brand = "Витязь";
+
+        tvPage.clickShowAllBrandsButton();
+        Thread.sleep(2000);
+        tvPage.clickTVBrandFilter(brand);
+        Thread.sleep(2000);
+
+        assertTrue(tvPage.areTVPageItemsContainText(brand));
+    }
+
+    @AfterEach
+    public void closeDriver() {
+        driver.quit();
+    }
+}
