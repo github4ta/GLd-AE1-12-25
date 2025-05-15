2f2b57cd3e67e3ebf14cefcb1db1e1b6c835506d
Aneta Romanovska <a.romanovska.dev@gmail.com>
2025-05-15 21:06:16 +0300

    UI0008

---
+++ src/main/java/by/vek/DetailsLocator.java
@@ -0,0 +1,8 @@
+package by.vek;
+
+public class DetailsLocator {
+    public static final String SUBMIT_BUTTON_COOKIE = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
+    public static final String ANCHOR_DETAILS = "//a[contains(@href, 'details')]";
+    public static final String HEADER_DETAILS = "//h1";
+    public static final String PARAGRAPH_DETAILS = "//div[@class=\"b-page\"]";
+}
