e62658cddedcb2d7025b89fd40c8caefb92fde07
Daniil.Vasilenka <daniil.vasilenko.10@mail.ru>
2025-05-15 19:13:46 +0300

    add-coffee

---
+++ src/test/java/CoffeeLocator.java
@@ -0,0 +1,6 @@
+public class CoffeeLocator {
+    public static final String COFFEE_LINK = "//a[contains(text(), 'Кофе')]";
+    public static final String COFFEE_HEADER = "//h1[contains(text(), 'Кофе')]";
+    public static final String COFFEE_TYPES = "//h2[contains(text(), 'Виды кофе')]";
+    public static final String COFFEE_SELECTION = "//h2[contains(text(), 'Какой кофе выбрать')]";
+}
---
+++ src/test/java/CoffeeTest.java
@@ -0,0 +1,44 @@
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CoffeeTest {
+    private  WebDriver driver;
+    private CoffeePage coffeePage;
+
+    @BeforeEach
+    public  void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get( "https://www.21vek.by");
+        coffeePage = new CoffeePage(driver);
+    }
+
+    @Test
+    public void verifyCoffeeSection() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement button = driver.findElement(By.linkText("Кофе"));
+        wait.until(ExpectedConditions.visibilityOf(button));
+        button.click();
+        Assert.assertTrue(coffeePage.isCoffeeHeaderDisplayed());
+        Assert.assertTrue(coffeePage.isCoffeeTypesDisplayed());
+        Assert.assertTrue(coffeePage.isCoffeeSelectionDisplayed());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
