1cfa467d8ca05314a203dd8beeb6c2318e512993
Yuliya <44757603+ChaoticHamka@users.noreply.github.com>
2025-05-15 21:07:38 +0300

    UI0007

---
+++ src/test/java/by/vek/VacuumTest.java
@@ -0,0 +1,44 @@
+package by.vek;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class VacuumTest {
+
+    private WebDriver driver;
+    private VacuumPage vacuumPage;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        driver.get("https://www.21vek.by");
+
+        vacuumPage = new VacuumPage(driver);
+        vacuumPage.clickSubmitButtonCookie();
+        vacuumPage.clickCatalogCategory();
+        vacuumPage.clickCategoryVacuum();
+
+    }
+
+    @Test
+    public void testVacuumPage() {
+        Assertions.assertEquals("Пылесосы", vacuumPage.getTitleVacuumText());
+        Assertions.assertEquals("Щетки, насадки для пылесосов", vacuumPage.getVacuumAttachmentsText());
+        Assertions.assertEquals("Расходные материалы для пылесосов", vacuumPage.getVacuumFiltersBagsText());
+        Assertions.assertEquals("Принадлежности для пылесосов", vacuumPage.getVacuumAccessoriesText());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
