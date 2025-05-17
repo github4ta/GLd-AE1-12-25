2a65920b159eb75845d595e369d304e83e29dc40
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-05-17 19:42:44 +0300

    add AuthorizationLocators, MainPage

---
+++ src/main/java/timoshenko/rw/by/AuthorizationLocators.java
@@ -0,0 +1,13 @@
+package timoshenko.rw.by;
+
+public class AuthorizationLocators {
+    public final static String TITLE_AUTHORIZATION = "//div[@class='auth-modal__title h3']";
+    public final static String COMMENT_LOGIN = "//div[text()='Логин/E-mail']";
+    public final static String COMMENT_PASSWORD = "//div[text()='Пароль']";
+    public final static String BUTTON_LOGIN = "//input[@class='btn btn-index btn_submit']";
+    public final static String LINK_FORGOT_PASSWORD = "//a[@href='/ru/registration/pwd_restore/']";
+    public final static String COMMENT_LOGIN_SOCIAL_MEDIA = "//div[@class='auth-modal__social-title']";
+    public final static String COMMENT_NO_ACCOUNT = "//div[@class='auth-modal__caption']";
+    public final static String LINK_REGISTRATION = "//a[@href='/ru/registration/']";
+
+}
