17dc8fe082801cb3134c496b053ffbcf05486baa
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-05-17 21:11:10 +0300

    add AuthorizationLocators, add ModalRecoveryLoginLocators, RecoveryLoginLocators, RegistrationLocators

--- src/main/java/timoshenko/rw/by/AuthorizationLocators.java
+++ src/main/java/timoshenko/rw/by/AuthorizationLocators.java
@@ -9,5 +9,10 @@ public class AuthorizationLocators {
     public final static String COMMENT_LOGIN_SOCIAL_MEDIA = "//div[@class='auth-modal__social-title']";
     public final static String COMMENT_NO_ACCOUNT = "//div[@class='auth-modal__caption']";
     public final static String LINK_REGISTRATION = "//a[@href='/ru/registration/']";
-
+    public final static String FIELD_INPUT_LOGIN = "//input[@name='login']";
+    public final static String FIELD_INPUT_PASSWORD = "//input[@name='password']";
+    public final static String COMMENT_LOGIN_ERROR = "//label[@id='login-error']";
+    public final static String COMMENT_PASSWORD_ERROR = "//label[@id='password-error']";
+    public final static String COMMENT_USER_NOT_FOUND = "//div[@class='danger standalone']";
+    public final static String COMMENT_INCORRECT_LOGIN_OR_PASSWORD = "//div[text()='Неверное имя пользователя или пароль']";
 }
---
+++ src/main/java/timoshenko/rw/by/RecoveryLoginLocators.java
@@ -0,0 +1,8 @@
+package timoshenko.rw.by;
+
+public class RecoveryLoginLocators {
+    public final static String TITLE_RECOVERY_PAGE = "//h1[@class='small']";
+    public final static String COMMENT_INFO_EMAIL = "//div[@class='registration__desc']";
+    public final static String COMMENT_EMAIL = "//div[text()='E-mail']";
+    public final static String BUTTON_RECOVERY = "//button[@class='btn btn-index btn_submit']";
+}
