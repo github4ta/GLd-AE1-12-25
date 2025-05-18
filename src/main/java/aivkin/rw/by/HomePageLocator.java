2b364e8c179e9c633346d8acbc08f78b696dfe82
Alexandr Ivkin <alexandr1ivkin@gmail.com>
2025-05-18 12:36:43 +0300

    add Class HomePageLocator

---
+++ src/main/java/aivkin/rw/by/HomePageLocator.java
@@ -0,0 +1,6 @@
+package aivkin.rw.by;
+
+public class HomePageLocator {
+    public static final String BUTTON_CLOSE_ALERT_WINDOW = "//div[@class=\"notification-modal__header\"]//button[@class=\"close close--black\"]";
+    public static final String BUTTON_PERSONAL_ACCOUNT = "//a[@data-target=\"#auth-popup\"]";
+}
