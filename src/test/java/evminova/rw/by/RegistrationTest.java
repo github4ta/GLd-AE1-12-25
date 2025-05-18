aefee99dce6ab7e50d030e231f679cf9ff0040e0
i.evminova <evminova.iryna@gmail.com>
2025-05-18 23:36:14 +0300

    add RegistrationTests

---
+++ src/test/java/evminova/rw/by/RegistrationTest.java
@@ -0,0 +1,65 @@
+package evminova.rw.by;
+
+import evminova.rw.by.driver.Driver;
+import evminova.rw.by.pages.homepage.HomePage;
+import evminova.rw.by.pages.registration.RegistrationPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegistrationTest {
+    private RegistrationPage registrationPage;
+    private HomePage homePage;
+
+    @BeforeEach
+    public void setUp() {
+        homePage = new HomePage();
+        homePage.open();
+        homePage.clickButtonCabinet();
+        registrationPage = new RegistrationPage();
+    }
+
+    @Test
+    @DisplayName("текст подписи поля Логин/E-mail")
+    public void testFieldNameLogin() {
+        assertEquals("Логин/E-mail", registrationPage.getTextFieldNameLogin());
+    }
+
+    @Test
+    @DisplayName("текст подписи поля «Пароль»")
+    public void testFieldNamePassword() {
+        assertEquals("Пароль", registrationPage.getTextFieldNamePassword());
+    }
+
+    @Test
+    @DisplayName("текст кнопки «ВОЙТИ»")
+    public void testTextButtonSubmit() {
+        assertEquals("ВОЙТИ", registrationPage.getTextButtonSubmit());
+    }
+
+    @Test
+    @DisplayName("текст ссылки «Забыли пароль?»")
+    public void testLinkTitleRestore() {
+        assertEquals("Забыли пароль?", registrationPage.getLinkTitleRestore());
+    }
+
+    @Test
+    @DisplayName("текст Войти с помощью соцсетей:")
+    public void testTitleSocialList() {
+        assertEquals("Войти с помощью соцсетей:\n", registrationPage.getTitleSocialList());
+    }
+
+    @Test
+    @DisplayName("текст «Нет аккаунта?»")
+    public void testTitleNoAccount() {
+        assertEquals("Нет аккаунта? ", registrationPage.getTitleNoAccount());
+    }
+
+    @AfterEach
+    public void quitAndNullDriver() {
+        Driver.quit();
+    }
+}
