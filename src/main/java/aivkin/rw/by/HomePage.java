d63ed4188f73e7410472496ecce8e4adc261e4cb
Alexandr Ivkin <alexandr1ivkin@gmail.com>
2025-05-18 13:09:34 +0300

    add Class HomePageLocator

---
+++ src/main/java/aivkin/rw/by/HomePage.java
@@ -0,0 +1,33 @@
+package aivkin.rw.by;
+
+import by.vek.driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    public HomePage() { driver = Driver.getDriver(); }
+
+    public void openHomePage() {
+        driver.get("https://pass.rw.by/ru/");
+        try {
+            closeModalWindow();
+            closeCookie();
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    private void closeModalWindow() {
+        driver.findElement(By.xpath(HomePageLocator.BUTTON_CLOSE_MODAL_WINDOW)).click();
+    }
+
+    private void closeCookie() {
+        driver.findElement(By.xpath(HomePageLocator.BUTTON_SUBMIT_COOKIE));
+    }
+
+    public void clickButtonPersonalAccount() {
+        driver.findElement(By.xpath(HomePageLocator.BUTTON_PERSONAL_ACCOUNT));
+    }
+}
