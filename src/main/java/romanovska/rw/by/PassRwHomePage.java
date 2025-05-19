91553b94487a1ce06dd50e969ecc1765e54eea33
a.romanovska <a.romanovska.dev@gmail.com>
2025-05-20 01:39:43 +0300

    update PassRwHomePage class

--- src/main/java/romanovska/rw/by/PassRwHomePage.java
+++ src/main/java/romanovska/rw/by/PassRwHomePage.java
@@ -1,26 +1,15 @@
 package romanovska.rw.by;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class PassRwHomePage extends BasePage {
     private final static String PROFILE_BUTTON = "//a[*[contains(@class, 'profile')]]";
     private final static String ACCEPT_COOKIE_BUTTON = "//div[@class='cookies-popup__btns']//button[contains(@class,'mc-btn-all')]";
     private final static String DO_NOT_SHOW_AGAIN_NOTIFICATION_CHECKBOX = "//div[@class='notification-modal__body']//label[*[contains(@class,'checkbox')]]";
 
-    WebDriverWait wait;
-
-    private static AuthorizationModal authorizationModal;
-
     public PassRwHomePage() {
         super();
         URL = "https://pass.rw.by/";
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(4));
     }
 
     @Override
@@ -28,45 +17,75 @@ public class PassRwHomePage extends BasePage {
         super.open();
 
         try {
-            wait.until(ExpectedConditions.elementToBeClickable(getDoNotShowAgainNotificationCheckbox()));
-
             clickDoNotShowAgain();
         } catch (Exception e) {
             System.out.println("Can't click 'Don't show again'");
         }
 
         try {
-            wait.until(ExpectedConditions.elementToBeClickable(getAcceptCookieButton()));
-
             clickAcceptCookie();
         } catch (Exception e) {
             System.out.println("Can't accept cookie");
         }
     }
 
-    private WebElement getDoNotShowAgainNotificationCheckbox() {
-        return driver.findElement(By.xpath(DO_NOT_SHOW_AGAIN_NOTIFICATION_CHECKBOX));
-    }
-
     public void clickDoNotShowAgain() {
-        driver.findElement(By.xpath(DO_NOT_SHOW_AGAIN_NOTIFICATION_CHECKBOX)).click();
+        getWebElementFoundByLocatorAfterWait(DO_NOT_SHOW_AGAIN_NOTIFICATION_CHECKBOX).click();
     }
 
     private WebElement getAcceptCookieButton() {
-        return driver.findElement(By.xpath(ACCEPT_COOKIE_BUTTON));
+        return getWebElementFoundByLocatorAfterWait(ACCEPT_COOKIE_BUTTON);
     }
 
     public void clickAcceptCookie() {
-        driver.findElement(By.xpath(ACCEPT_COOKIE_BUTTON)).click();
+        getWebElementFoundByLocatorAfterWait(ACCEPT_COOKIE_BUTTON).click();
     }
 
     public void clickProfile() {
-        driver.findElement(By.xpath(PROFILE_BUTTON)).click();
-        authorizationModal = new AuthorizationModal();
+        getWebElementFoundByLocatorAfterWait(PROFILE_BUTTON).click();
     }
 
     public String getAuthorizationModalHeaderText() {
-        System.out.println("хоумпейдж отправяет: "+authorizationModal.getHeaderText(driver));
-        return authorizationModal.getHeaderText(driver);
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.HEADER_TITLE).getText();
+    }
+
+    public String getAuthorizationModalLoginOrEmailFieldNameText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.LOGIN_OR_EMAIL_FIELD_NAME).getText();
+    }
+
+    public String getAuthorizationModalPasswordFieldNameText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.PASSWORD_FIELD_NAME).getText();
+    }
+
+    public String getAuthorizationModalLoginButtonText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.LOGIN_BUTTON).getAttribute("value");
+    }
+
+    public String getAuthorizationModalForgotPasswordLinkText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.FORGOT_PASSWORD_LINK).getText();
+    }
+
+    public String getAuthorizationModalLoginViaSocialTitleText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.LOGIN_VIA_SOCIAL_TITLE).getText();
+    }
+
+    public String getAuthorizationModalNoAccountYetCaptionText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.NO_ACCOUNT_YET_CAPTION).getText();
+    }
+
+    public String getAuthorizationModalRegisterLinkText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.REGISTER_LINK).getText();
+    }
+
+    public void fillLoginOrEmailField(String loginOrEmail) {
+        getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.LOGIN_OR_EMAIL_FIELD).sendKeys(loginOrEmail);
+    }
+
+    public void clickLoginButton() {
+        getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.LOGIN_BUTTON).click();
+    }
+
+    public String getAuthorizationModalPasswordErrorText() {
+        return getWebElementFoundByLocatorAfterWait(AuthorizationModalLocators.PASSWORD_ERROR_LABEL).getText();
     }
 }
