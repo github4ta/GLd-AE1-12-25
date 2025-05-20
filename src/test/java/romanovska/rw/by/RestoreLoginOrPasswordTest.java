764d98ee5790099c6fa7d3886796282029118b14
a.romanovska <a.romanovska.dev@gmail.com>
2025-05-20 15:52:49 +0300

    refactor RestoreLoginOrPasswordTest

--- src/test/java/romanovska/rw/by/RestoreLoginOrPasswordTest.java
+++ src/test/java/romanovska/rw/by/RestoreLoginOrPasswordTest.java
@@ -1,5 +1,27 @@
 package romanovska.rw.by;
 
+import by.vek.driver.Driver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import romanovska.rw.by.pages.RestoreLoginOrPasswordPage;
+
 public class RestoreLoginOrPasswordTest {
-    RestoreLoginOrPasswordPage restorePage;
+    private final String HEADER_TEXT_CORRECT = "Восстановление логина или пароля";
+
+    @BeforeEach
+    public void openRestoreLoginOrPasswordPage() {
+        TestUtils.openRestoreLoginOrPasswordPage();
+    }
+
+    @Test
+    public void testContent() {
+        Assertions.assertEquals(HEADER_TEXT_CORRECT, RestoreLoginOrPasswordPage.getHeaderText());
+    }
+
+    @AfterEach
+    public void quitDriver() {
+        Driver.quit();
+    }
 }
