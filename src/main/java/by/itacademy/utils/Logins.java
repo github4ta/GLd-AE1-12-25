beed05f44e551bde454d6dca29390efd410652cf
romankuleshov <kuleshov.roman96@gmail.com>
2025-05-22 20:04:10 +0300

    refactoring Logins

--- src/main/java/by/itacademy/utils/Logins.java
+++ src/main/java/by/itacademy/utils/Logins.java
@@ -5,12 +5,24 @@ public class Logins {
     }
 
     public static String generateCorrectLogin() {
-        String correctLogin = "";
-        int numberOfSymbols = (int) (Math.random() * 14 + 3);
         String correctSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        return generateLogin(correctSymbols, 3, 16);
+    }
+
+    public static String generateIncorrectLogin() {
+        String correctSymbols = "!@#$%^&*";
+
+        return generateLogin(correctSymbols, 3, 16);
+    }
+
+    private static String generateLogin(String symbols, int minimumLength, int maximumLength) {
+        String login = "";
+        int numberOfSymbols = (int) (Math.random() * (maximumLength - minimumLength + 1) + minimumLength);
+
         for (int i = 0; i < numberOfSymbols; i++) {
-            correctLogin += correctSymbols.charAt((int)(Math.random() * correctSymbols.length()));
+            login += symbols.charAt((int) (Math.random() * symbols.length()));
         }
-        return correctLogin;
+        return login;
     }
 }
