022d51e787e3df257eda95ec8d78d3733c357492
Lexzorg1990 <alexandr.pesetskiy@gmail.com>
2025-05-25 00:19:17 +0300

    add update Locator link Smartphone

--- src/main/java/by/vek/pages/smartphone/SmartphoneLocator.java
+++ src/main/java/by/vek/pages/smartphone/SmartphoneLocator.java
@@ -2,7 +2,7 @@ package by.vek.pages.smartphone;
 
 public class SmartphoneLocator {
     public static final String BUTTON_ACCEPT_COOKIE = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
-    public static final String SMARTPHONE_LINK = "//*[@id=\"header\"]/div/div[3]/div/div/div[2]/ul/li[10]/a";
+    public static final String SMARTPHONE_LINK = "//div[@class=\"styles_promoList__yozMt\"]//a[@href=\"/mobile/\"]";
     public static final String SMARTPHONE_HEADER = "//h1[@class=\"style_title__QomyX\"]";
     public static final String SMARTPHONE_OR_MOBILE = "//h2[contains(text(), 'Смартфон или мобильный телефон')]";
     public static final String SMARTPHONE_CHOOSE = "//h2[contains(text(), 'Какой телефон выбрать')]";
