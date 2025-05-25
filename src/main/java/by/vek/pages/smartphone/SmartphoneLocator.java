ace1b66b49b98ff057b3d19a29bfc7491567a597
Lexzorg1990 <alexandr.pesetskiy@gmail.com>
2025-05-25 15:56:47 +0300

    refactor

---
+++ src/main/java/by/vek/pages/smartphone/SmartphoneLocator.java
@@ -0,0 +1,9 @@
+package by.vek.pages.smartphone;
+
+public class SmartphoneLocator {
+    public static final String BUTTON_ACCEPT_COOKIE = "//button[@class=\"Button-module__button Button-module__blue-primary\"]";
+    public static final String BUTTON_SMARTPHONE = "//div[@class=\"styles_promoList__yozMt\"]//a[@href=\"/mobile/\"]";
+    public static final String HEADER_SMARTPHONE = "//h1[@class=\"style_title__QomyX\"]";
+    public static final String TITLE_SMARTPHONE_OR_MOBILE = "//h2[contains(text(), 'Смартфон или мобильный телефон')]";
+    public static final String TITLE_SMARTPHONE_CHOOSE = "//h2[contains(text(), 'Какой телефон выбрать')]";
+}
