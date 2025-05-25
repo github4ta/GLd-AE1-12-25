c1f1107716e7142c5b5d97ac86d9634c6753c4f7
Lexzorg1990 <alexandr.pesetskiy@gmail.com>
2025-05-25 17:40:36 +0300

    add class BasePage

--- src/main/java/pesetskiy/rw/by/pages/BasePage.java
+++ src/main/java/pesetskiy/rw/by/pages/BasePage.java
@@ -1,4 +1,28 @@
 package pesetskiy.rw.by.pages;
 
+import by.vek.driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class BasePage {
+
+    private static final String BUTTON_ACCEPT_COOKIE = "//button[@class=\"btn btn-default mc-btn-all\"]";
+
+    protected WebDriver driver;
+
+    public BasePage() {
+        driver = Driver.getDriver();
+    }
+
+    public void open() {
+        driver.get( "https://pass.rw.by/ru/");
+        try {
+            clickButtonAcceptCookie();
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void clickButtonAcceptCookie() {
+        driver.findElement(By.xpath(BUTTON_ACCEPT_COOKIE)).click();
+    }
 }
