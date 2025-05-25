36b8cfae80655d212d2afb03c84fc19bd12719d0
Lexzorg1990 <alexandr.pesetskiy@gmail.com>
2025-05-25 21:18:59 +0300

    add class AuthorizationFormPage

--- src/main/java/pesetskiy/rw/by/pages/authorizationForm/AuthorizationFormPage.java
+++ src/main/java/pesetskiy/rw/by/pages/authorizationForm/AuthorizationFormPage.java
@@ -1,4 +1,75 @@
 package pesetskiy.rw.by.pages.authorizationForm;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pesetskiy.rw.by.driver.Driver;
+
 public class AuthorizationFormPage {
+
+    private final WebDriver driver;
+
+    public AuthorizationFormPage() {
+        driver = Driver.getDriver();
+    }
+
+    public String getHeaderAuthorizationText(){
+        WebElement headerAuthorizationText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_HEADER_AUTHORIZATION));
+        return headerAuthorizationText.getText();
+    }
+
+    public String getLabelLoginText(){
+        WebElement labelLoginText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_LOGIN));
+        return labelLoginText.getText();
+    }
+
+    public String getLabelPasswordText(){
+        WebElement labelPasswordText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_PASSWORD));
+        return labelPasswordText.getText();
+    }
+
+    public String getButtonLoginText(){
+        WebElement buttonLoginText = driver.findElement(By.xpath(AuthorizationFormLocator.BUTTON_LOGIN));
+        return buttonLoginText.getText();
+    }
+
+    public String getLinkForgotPasswordText(){
+        WebElement linkForgotPasswordText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LINK_FORGOT_PASSWORD));
+        return linkForgotPasswordText.getText();
+    }
+
+    public String getLabelOtherLoginText(){
+        WebElement labelOtherLoginText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_OTHER_LOGIN));
+        return labelOtherLoginText.getText();
+    }
+
+    public String getLabelNoAccountText(){
+        WebElement labelNoAccountText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_NO_ACCOUNT));
+        return labelNoAccountText.getText();
+    }
+
+    public String getLinkRegistrationText(){
+        WebElement linkRegistrationText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LINK_REGISTRATION));
+        return linkRegistrationText.getText();
+    }
+
+    public String getLabelNoLoginText(){
+        WebElement labelNoLoginText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_NO_LOGIN));
+        return labelNoLoginText.getText();
+    }
+
+    public String getLabelNoPasswordText(){
+        WebElement labelNoPasswordText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_NO_PASSWORD));
+        return labelNoPasswordText.getText();
+    }
+
+    public String getLabelUserNotFoundText(){
+        WebElement labelUserNotFoundText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_USER_NOT_FOUND));
+        return labelUserNotFoundText.getText();
+    }
+
+    public String getLabelIncorrectLoginOrPasswordText(){
+        WebElement labelIncorrectLoginOrPasswordText = driver.findElement(By.xpath(AuthorizationFormLocator.TITLE_LABEL_INCORRECT_LOGIN_OR_PASSWORD));
+        return labelIncorrectLoginOrPasswordText.getText();
+    }
 }
--- src/test/java/pesetskiy/rw/by/AuthorizationFormTest.java
+++ src/test/java/pesetskiy/rw/by/AuthorizationFormTest.java
@@ -1,4 +1,20 @@
 package pesetskiy.rw.by;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pesetskiy.rw.by.driver.Driver;
+import pesetskiy.rw.by.pages.authorizationForm.AuthorizationFormPage;
+
 public class AuthorizationFormTest {
+
+    private AuthorizationFormPage authorizationFormPage;
+
+    @Test
+    @DisplayName("Check authorization form")
+
+    @AfterEach
+    public void tearDown() {
+        Driver.quit();
+    }
 }
