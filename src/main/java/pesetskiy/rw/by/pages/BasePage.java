d9792a64211d0af1d50f1a4d8863a9ef6f728900
Lexzorg1990 <alexandr.pesetskiy@gmail.com>
2025-05-25 20:18:49 +0300

    add class AuthorizationFormLocator

--- src/main/java/pesetskiy/rw/by/pages/BasePage.java
+++ src/main/java/pesetskiy/rw/by/pages/BasePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 public class BasePage {
 
     private static final String BUTTON_ACCEPT_COOKIE = "//button[@class=\"btn btn-default mc-btn-all\"]";
+    private static final String BUTTON_CABINET = "//li[@class=\"item_1 cabinet\"]";
 
     protected WebDriver driver;
 
@@ -18,11 +19,16 @@ public class BasePage {
         driver.get( "https://pass.rw.by/ru/");
         try {
             clickButtonAcceptCookie();
+            clickButtonCabinet();
         } catch (Exception ignored) {
         }
     }
 
     public void clickButtonAcceptCookie() {
         driver.findElement(By.xpath(BUTTON_ACCEPT_COOKIE)).click();
+    }
+
+    public void clickButtonCabinet() {
+        driver.findElement(By.xpath(BUTTON_CABINET)).click();
     }
 }
--- src/main/java/pesetskiy/rw/by/pages/authorizationForm/AuthorizationFormLocator.java
+++ src/main/java/pesetskiy/rw/by/pages/authorizationForm/AuthorizationFormLocator.java
@@ -1,4 +1,16 @@
 package pesetskiy.rw.by.pages.authorizationForm;
 
 public class AuthorizationFormLocator {
+    public final static String TITLE_HEADER_AUTHORIZATION = "//div[@class='auth-modal__title h3']";
+    public final static String TITLE_LABEL_LOGIN = "//div[contains(text(), 'Логин/E-mail')]";
+    public final static String TITLE_LABEL_PASSWORD = "//div[contains(text(), 'Пароль')]";
+    public final static String BUTTON_LOGIN = "//input[@class=\"btn btn-index btn_submit\"]";
+    public final static String TITLE_LINK_FORGOT_PASSWORD = "//a[@href=\"/ru/registration/pwd_restore/\"]";
+    public final static String TITLE_LABEL_OTHER_LOGIN = "//div[@class=\"auth-modal__social-title\"]";
+    public final static String TITLE_LABEL_NO_ACCOUNT = "//div[contains(text(), 'Нет аккаунта?')]";
+    public final static String TITLE_LINK_REGISTRATION = "//a[@href=\"/ru/registration/\"]";
+    public final static String TITLE_LABEL_NO_LOGIN = "//label[@id=\"login-error\"]";
+    public final static String TITLE_LABEL_NO_PASSWORD = "//label[@id=\"password-error\"]";
+    public final static String TITLE_LABEL_USER_NOT_FOUND = "//div[contains(text(), 'Пользователь не найден')]";
+    public final static String TITLE_LABEL_INCORRECT_LOGIN_OR_PASSWORD = "//div[contains(text(), 'Неверное имя пользователя или пароль')]";
 }
