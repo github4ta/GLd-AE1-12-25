5afa73eabb396064a14f64deea7f7470ecbf73f4
Victor Sikachyov <3510154@gmail.com>
2025-05-27 20:30:47 +0300

    add API test nasha moda

---
+++ src/test/java/by/nashamoda/api/LoginTest.java
@@ -0,0 +1,19 @@
+package by.nashamoda.api;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() {
+        given()
+                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("request_header%5Bmobile%5D", "0")
+                .formParam("email", "test1%40test.com")
+                .formParam("password", "fwrew")
+                .when().post("https://www.nashamoda.by/login?act=login")
+                .then().log().all();
+    }
+}
