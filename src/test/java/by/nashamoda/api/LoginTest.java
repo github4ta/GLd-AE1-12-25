c088c9894c556685ca5118873cb18fac33c5ef0f
Sergei Tsarik <serge.tsarik@gmail.com>
2025-05-27 21:54:09 +0300

    refactor nasha moda

--- src/test/java/by/nashamoda/api/LoginTest.java
+++ src/test/java/by/nashamoda/api/LoginTest.java
@@ -2,15 +2,26 @@ package by.nashamoda.api;
 
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LoginTest {
 
+    private LoginPage loginPage;
+
+    @BeforeEach
+    public void setUp() {
+        Faker faker = new Faker();
+        loginPage = new LoginPage(faker.internet().emailAddress(), faker.internet().password());
+    }
+
     @Test
     public void loginTest() {
-        Faker faker = new Faker();
-        LoginPage loginPage = new LoginPage(faker.internet().emailAddress(), faker.internet().password());
-
         Assertions.assertEquals(300, loginPage.getStatusCode());
+    }
+
+    @Test
+    public void loginTest2() {
+        Assertions.assertEquals(200, loginPage.getStatusCode());
     }
 }
