c36d764726ac66bfe8bf43d0da1de4a320644763
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-05-29 20:57:33 +0300

    refactor LoginPage LoginTest

--- src/main/java/by/edostavka/api/LoginPage.java
+++ src/main/java/by/edostavka/api/LoginPage.java
@@ -1,4 +1,35 @@
 package by.edostavka.api;
 
+import io.restassured.response.Response;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
 public class LoginPage {
+
+    private Response response;
+    public LoginPage(){
+        response = getResponse();
+    }
+
+    private Response getResponse() {
+        Response response = given()
+                .header("apitoken", "4Pg2vLanS2Zdb9Pa2SAdZxEUPdNBEpiE")
+                .header("web-user-agent", "SiteEdostavka/1.0.0")
+                .header("content-type", "application/json")
+                .body("{\"phone\":\"\",\"password\":\"qwerty\"}")
+                .when().post("https://api2.edostavka.by/api/v2/auth");
+        return response;
+    }
+
+    public int getStatusCode(){
+        Response response = getResponse();
+        return response.getStatusCode();
+    }
+
+    public List<String> getInvalidField(){
+        Response response = getResponse();
+        return response.path("invalidField");
+    }
 }
--- src/test/java/by/edostavka/api/LoginTest.java
+++ src/test/java/by/edostavka/api/LoginTest.java
@@ -1,24 +1,23 @@
 package by.edostavka.api;
 
-import io.restassured.specification.RequestSpecification;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.given;
 
 public class LoginTest {
 
     @Test
-    public void loginTest(){
-        given()
-                .header("apitoken","4Pg2vLanS2Zdb9Pa2SAdZxEUPdNBEpiE")
-                .header("web-user-agent", "SiteEdostavka/1.0.0")
-                .header("content-type", "application/json")
-                .body("{\"phone\":\"\",\"password\":\"qwerty\"}")
-//                .formParam("phone", "")
-//                .formParam("password", "")
-                .when().post("https://api2.edostavka.by/api/v2/auth")
-//                .then().statusCode(423);
-                .then().log().all();
+    public void testLoginStatusCode() {
 
-        RequestSpecification request = given();
+        LoginPage loginPage = new LoginPage();
+        int actualStatusCOde = loginPage.getStatusCode();
+        Assertions.assertEquals(422, actualStatusCOde);
+    }
+
+    @Test
+    public void testText(){
+        LoginPage loginPage = new LoginPage();
+        Assertions.assertEquals("phone", loginPage.getInvalidField());
+        System.out.println(loginPage.getInvalidField());
     }
 }
