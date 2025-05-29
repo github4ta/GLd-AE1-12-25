baab7af1cbcb41e64a476dc3cb70c4d1947a6d54
a.romanovska <a.romanovska.dev@gmail.com>
2025-05-29 21:49:32 +0300

    refactor sporcle

--- src/main/java/com/sporcle/api/LoginPage.java
+++ src/main/java/com/sporcle/api/LoginPage.java
@@ -5,21 +5,20 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 
 public class LoginPage {
-    private Response response;
+    private final Response response;
 
     public LoginPage() {
         response = getResponse();
     }
 
     private Response getResponse() {
-        Response response = given()
+        return given()
                 .header("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
                 //.body("email=test%40test.xyz&passwd=7298472839432&remember=1")
                 .formParam("email", "test%40test.xyz")
                 .formParam("passwd", "7298472839432")
                 .formParam("remember", 1)
                 .when().post("https://www.sporcle.com/auth/ajax/login.php");
-        return response;
     }
 
     public int getStatusCode() {
@@ -30,11 +29,11 @@ public class LoginPage {
         return response.path("error_text");
     }
 
-    public String getErrorsEmailMessage(){
+    public String getErrorsEmailMessage() {
         return response.path("errors[0].message");
     }
 
-    public String getErrorsEmailField(){
+    public String getErrorsEmailField() {
         return response.path("errors[0].field");
     }
 }
